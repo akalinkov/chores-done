1ba6183ac5944f5f37515a0d22d5329c531267a9
Aliaksei Kalinkou <aliaksei.kalinkou@gmail.com>
2018-09-27 20:38:16 -0400

    Initial commit

---
+++ app/src/main/java/com/akalinkou/choresdone/SelectUserActivity.java
@@ -0,0 +1,13 @@
+package com.akalinkou.choresdone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_user);
+    }
+}
